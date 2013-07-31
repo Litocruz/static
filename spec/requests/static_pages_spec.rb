5b93d6daf83f9b69f72942b947dcb14f8f9ddfda
Litocruz <rengolamadrid@hotmail.com>
2013-07-31 19:18:10 -0300

    Make a basic User Model (including secure password)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,29 +5,53 @@ describe "StaticPages" do
   #let(:base_title) {"Ruby on Rails"}
   subject { page }
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should # fill in
+    click_link "Contact"
+    page.should # fill in
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should # fill in
+    click_link "sample app"
+    page.should # fill in
+  end
+
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home Page" do
     before {visit root_path}
-
-    it { should have_selector('h1', text: 'Home')}
-    it { should have_selector('title', text: full_title(''))}
+    let(:heading) {'Home'}
+    let(:page_title) {''}
+    it_should_behave_like "all static pages"
     it { should_not have_selector('title', text: '| SampleApp')}
   end
 
   describe "Help Page" do
     before {visit help_path}
-    it { should have_selector('h1', text: 'Help')}
-    it { should have_selector('title', text: full_title(''))}
+    let(:heading) {'Help'}
+    let(:page_title) {''}
+    it_should_behave_like "all static pages"
   end
 
   describe "About Page" do
     before {visit about_path}
-    it { should have_selector('h1', text: 'About Us')}
-    it { should have_selector('title', text: full_title(''))}
+    let(:heading) {'About Us'}
+    let(:page_title) {''}
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before {visit contact_path}
-    it { page.should have_selector('h1', text: 'Contact')}
-    it { should have_selector('title', text: full_title(''))}
+    let(:heading) {'Contact'}
+    let(:page_title) {''}
+    it_should_behave_like "all static pages"
   end
 end
