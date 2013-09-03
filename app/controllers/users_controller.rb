045b4e54cd80987b6d762c218dcf899dca948df1
Julian Lamadrid <jelamadrid@e-din.com.ar>
2013-09-03 11:38:25 -0300

    Manipulating microposts unidad 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,15 +49,6 @@ class UsersController < ApplicationController
   end
 
   private
-    def signed_in_user
-      #is equivalent to:
-      #flash[:notice] = "Please sign in."
-      #redirect to signin path
-      unless signed_in?
-        store_location
-        redirect_to signin_path, notice: "Please sign in." unless signed_in?
-      end
-    end
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Static::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
 
   #resources :home
   root to: 'static_pages#home'
